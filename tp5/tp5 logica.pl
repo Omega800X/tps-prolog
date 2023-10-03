0c36348d6c978cb628d1611f2742cc9444282130
Fernando Medvedovsky <fernandodavidmedve56@gmail.com>
2023-10-02 22:55:08 -0300

    punto 5 a) b) c) d)

--- tp5/tp5 logica.pl
+++ tp5/tp5 logica.pl
@@ -18,3 +18,28 @@ eliminaTodos(X, [Y|Ys], [Y | Zs]) :- eliminaTodos(X, Ys, Zs).
 % Ls.
 longitud([], 0).
 longitud([_| COLA], RES) :- longitud(COLA, P), RES is 1 + P.
+
+% Punto 4
+% Utilizar un predicado concatenar(Xs, Ys, Zs). Donde Xs y Ys son las listas a concatenar
+% y Zs es la lista obtenida como resultado de la concatenación.
+concatenar([],Ys,Ys).
+concatenar([X|Xs],Ys,[X|Zs]):- concatenar(Xs, Ys, Zs).
+
+% Punto 5
+% Utilizando el predicado concatenar/3, definir las siguientes reglas:
+
+% a) prefijo(Xs, Ys): indica si los elementos de Xs están al principio de Ys.
+prefijo(Xs, Ys) :- concatenar(Xs, _, Ys).
+
+% b) sufijo(Xs, Ys): indica si los elementos de Xs están al final de Ys
+sufijo(Xs, Ys) :- concatenar(_, Xs, Ys).
+
+% c) sublista(Xs, Ys): indica si Xs es una sublista de Ys.
+sublista(Xs, Ys) :- concatenar(_, SUBSTRING, Ys), concatenar(Xs, _, SUBSTRING).
+
+% d) pertenece(X, Xs): si X es un elemento de Xs.
+pertenece(X, Xs) :- sublista([X], Xs).
+
+% e) invertirL (Xs, Ys): Ys es Xs con los elementos en orden inverso.
+
+
